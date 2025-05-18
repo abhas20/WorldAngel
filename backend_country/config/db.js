a0f31d94255b9ebcef5c13f6575fc4874811a57f
abhas20 <darklegends2971@gmail.com>
2025-05-18 22:24:19 +0530

    updated config

--- backend_country/config/db.js
+++ backend_country/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const connectDB=async ()=>{
-    await mongoose.connect(process.env.DB_URL);
+    await mongoose.connect(`${process.env.DB_URL}${process.env.DB_NAME}`);
     console.log("connected Successfully to DB");
 }
 
